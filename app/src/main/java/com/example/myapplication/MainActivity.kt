af245d0b82bc6146a69398f9a999967709a1d84a
oscarAndroid <oscarlimachitapia@gmail.com>
2025-03-18 19:44:56 +0100

    cambios

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,15 +11,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var weightEditText: EditText
-    lateinit var heightEditText: EditText
+    //weight
+    lateinit var removeWeightButton: Button
+    lateinit var addWeightButton: Button
+    lateinit var weightTextView: TextView
+    //height
+    lateinit var heightSlider: Slider
+    lateinit var heightTextView: TextView
+    //button
     lateinit var calculateButton: Button
+    //resultado
     lateinit var resultTextView: TextView
-
+    //Datos predeterminados
+    var weight: Float = 74.0f
+    var height: Float = 170.0f
 
     @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,21 +42,38 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        weightEditText=findViewById(R.id.weightEditText)
-        heightEditText=findViewById(R.id.heightEditText)
+        //weight
+        removeWeightButton=findViewById(R.id.removeWeightButton)
+        addWeightButton=findViewById(R.id.addWeightButton)
+        weightTextView=findViewById(R.id.weightTextView)
+        //height
+        heightTextView=findViewById(R.id.heightTextView)
+        heightSlider=findViewById(R.id.heightSlider)
+        //button
         calculateButton=findViewById(R.id.calculateButton)
+        //resultado
         resultTextView=findViewById(R.id.resultTextView)
 
-        calculateButton.setOnClickListener {
-            val weight = weightEditText.text.toString().toFloat()
-            val height= heightEditText.text.toString().toFloat()
-            val result = weight/((height/100).pow(2))
-
-            resultTextView.text= String.format("%.2f", result)
+// -------------Empieza la programación
 
 
+            removeWeightButton.setOnClickListener {
+                weight--
+                weightTextView.text="${weight.toInt()} kg"
+            }
+            addWeightButton.setOnClickListener {
+                weight++
+                weightTextView.text="${weight.toInt()} kg"
+            }
+            heightSlider.addOnChangeListener { slider, value, fromUser ->
+                height=value
+                heightTextView.text="${height.toInt()} cm"
+            }
+            calculateButton.setOnClickListener {
+                val result=weight/(height/100).pow(2)
+                resultTextView.text=String.format("%.2f", result)
+            }
 
-        }
+
     }
 }
